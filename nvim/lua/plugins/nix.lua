9515ef2f863805aef421315bbd328c2ff4cc7b61
ChanningHe <channinghey@gmail.com>
2026-01-02 19:21:29 +0900

    Add nix lsp && update lazy-lock.json

---
+++ nvim/lua/plugins/nix.lua
@@ -0,0 +1,17 @@
+return {
+  -- add svelte to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "nix" })
+    end,
+  },
+
+  -- correctly setup mason lsp / dap extensions
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "nil" })
+    end,
+  },
+}
